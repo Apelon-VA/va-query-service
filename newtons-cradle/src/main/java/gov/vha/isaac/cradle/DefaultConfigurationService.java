c182d9b043b3e6d1fc5a06aff5ddb6a2c743a5c8
darmbrust <daniel.armbrust.list@gmail.com>
2015-04-23 16:00:43 -0500

    fix two bugs I introduced

--- newtons-cradle/src/main/java/gov/vha/isaac/cradle/DefaultConfigurationService.java
+++ newtons-cradle/src/main/java/gov/vha/isaac/cradle/DefaultConfigurationService.java
@@ -92,7 +92,7 @@ public class DefaultConfigurationService implements ConfigurationService
 				}
 			}
 		}
-		return Optional.of(dataStoreFolderPath_);
+		return Optional.ofNullable(dataStoreFolderPath_);
 	}
 
 	/**
@@ -107,16 +107,15 @@ public class DefaultConfigurationService implements ConfigurationService
 		{
 			throw new IllegalStateException("Can only set the dbFolderPath prior to starting Isaac");
 		}
-
+		
 		if (Files.exists(dataStoreFolderPath) && !Files.isDirectory(dataStoreFolderPath))
 		{
 			throw new IllegalArgumentException("The specified path to the db folder appears to be a file, rather than a folder, as expected.  " + " Found: "
 					+ dataStoreFolderPath_.toAbsolutePath().toString());
 		}
-		
 		try
 		{
-			Files.createDirectories(dataStoreFolderPath_);
+			Files.createDirectories(dataStoreFolderPath);
 		}
 		catch (IOException e)
 		{
