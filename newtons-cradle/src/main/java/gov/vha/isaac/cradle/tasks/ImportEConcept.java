b6d379f7e13ffc84cce55e26dfc2a71d31e51ae4
darmbrust <daniel.armbrust.list@gmail.com>
2015-05-27 00:56:33 -0500

    handle per-concept locking during eConcept import on merge

--- newtons-cradle/src/main/java/gov/vha/isaac/cradle/tasks/ImportEConcept.java
+++ newtons-cradle/src/main/java/gov/vha/isaac/cradle/tasks/ImportEConcept.java
@@ -15,6 +15,7 @@ import java.util.UUID;
 import java.util.concurrent.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.ihtsdo.otf.tcc.api.concurrency.ConcurrentReentrantLocks;
 import org.ihtsdo.otf.tcc.api.coordinate.Status;
 import org.ihtsdo.otf.tcc.api.metadata.binding.Snomed;
 import org.ihtsdo.otf.tcc.lookup.Hk2Looker;
@@ -36,6 +37,7 @@ public class ImportEConcept implements Callable<Void> {
     private static final int inferredNid;
     public static CasSequenceObjectMap<TaxonomyRecordPrimitive> originDestinationTaxonomyRecords;
     public static ConcurrentSkipListSet<DestinationOriginRecord> destinationOriginRecordSet;
+    private static ConcurrentReentrantLocks conceptLockPool;
 
     static {
         originDestinationTaxonomyRecords = cradle.getOriginDestinationTaxonomyMap();
@@ -43,6 +45,7 @@ public class ImportEConcept implements Callable<Void> {
         isaNid = cradle.getNidForUuids(IsaacMetadataAuxiliaryBinding.IS_A.getUuids());
         statedNid = cradle.getNidForUuids(IsaacMetadataAuxiliaryBinding.STATED.getUuids());
         inferredNid = cradle.getNidForUuids(IsaacMetadataAuxiliaryBinding.INFERRED.getUuids());
+        conceptLockPool = new ConcurrentReentrantLocks(128);
     }
 
     TtkConceptChronicle eConcept;
@@ -62,12 +65,20 @@ public class ImportEConcept implements Callable<Void> {
     @Override
     public Void call() throws Exception {
 
+        Integer lockId = null;
         try {
             if (this.newPathUuid != null) {
                 eConcept.processComponentRevisions(r -> r.setPathUuid(newPathUuid));
             }
 
             int conceptNid = cradle.getNidForUuids(eConcept.getPrimordialUuid());
+            lockId = eConcept.getPrimordialUuid().hashCode();
+            //Note that this isn't 100% correct, if one were trying to do a merge on two concepts that had different primordial UUIDs (but shared alternate IDs)
+            //and were intended to be merged.  Not sure how we generated a lock ID for that case... this handles the cases I need handled at the moment... as 
+            //the merge cases I have all involve concepts where the UUIDs are specified in the same order, so this lock blocks multi-thread corruption when an 
+            //eConcept file lists the same concept multiple times (and requires merging)
+            conceptLockPool.lock(lockId);
+            
             cradle.setConceptNidForNid(conceptNid, conceptNid);
             ConceptChronicle cc = ConceptChronicle.get(conceptNid);
             ConceptChronicle.mergeWithEConcept(eConcept, cc);
@@ -138,6 +149,11 @@ public class ImportEConcept implements Callable<Void> {
         } catch (Exception e) {
             System.err.println("Failure importing " + eConcept.toString());
             throw e;
+        }
+        finally {
+            if (lockId != null) {
+                conceptLockPool.unlock(lockId);
+            }
         }
     }
 
