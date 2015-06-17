1aef6c89c88d9a54e1c7da45e2941541ca366719
darmbrust <daniel.armbrust.list@gmail.com>
2015-06-17 02:13:02 -0500

    hack code to deal with a sememe bug, fixed the concept index loop to pass in descriptions, rather than concepts, to the indexers.

--- newtons-cradle/src/main/java/gov/vha/isaac/cradle/tasks/GenerateIndexes.java
+++ newtons-cradle/src/main/java/gov/vha/isaac/cradle/tasks/GenerateIndexes.java
@@ -19,6 +19,7 @@ import javafx.concurrent.Task;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.ihtsdo.otf.lookup.contracts.contracts.ActiveTaskSet;
+import org.ihtsdo.otf.tcc.api.refexDynamic.RefexDynamicChronicleBI;
 import org.ihtsdo.otf.tcc.model.cc.refex.RefexMember;
 import org.ihtsdo.otf.tcc.model.cc.refex.RefexService;
 import org.ihtsdo.otf.tcc.model.index.service.IndexStatusListenerBI;
@@ -30,7 +31,7 @@ import org.ihtsdo.otf.tcc.model.index.service.IndexerBI;
  */
 public class GenerateIndexes extends Task<Void> {
     private final static IdentifierService idProvider = LookupService.getService(IdentifierService.class);
-    private final static RefexService refexProvider = LookupService.getService(RefexService.class);
+//    private final static RefexService refexProvider = LookupService.getService(RefexService.class);
     private final static SememeService sememeProvider = LookupService.getService(SememeService.class);
     private final static ConceptService conceptService = LookupService.getService(ConceptServiceManagerI.class).get();
 
@@ -76,39 +77,56 @@ public class GenerateIndexes extends Task<Void> {
             log.info("Clearing index for: " + i.getIndexerName());
             i.clearIndex();
         });
-        int conceptCount = conceptService.getConceptCount();
-        log.info("Concepts to index: " + conceptCount);
-        int refexCount = (int) idProvider.getRefexSequenceStream().count();
-        log.info("Refexes to index: " + refexCount);
-        int sememeCount = (int) idProvider.getSememeSequenceStream().count();
-        log.info("Sememes to index: " + sememeCount);
-        componentCount = conceptCount + refexCount + sememeCount;
-        log.info("Components to index: " + componentCount);
+
     }
 
     @Override
     protected Void call() throws Exception {
         LookupService.get().getService(ActiveTaskSet.class).get().add(this);
         try {
+            //TODO performance problem... all of these count methods are incredibly slow
+            int conceptCount = conceptService.getConceptCount();
+            log.info("Concepts to index: " + conceptCount);
+//            long refexCount = (int) idProvider.getRefexSequenceStream().count();
+//            log.info("Refexes to index: " + refexCount);
+            long sememeCount = (int) idProvider.getSememeSequenceStream().count();
+            log.info("Sememes to index: " + sememeCount);
+            componentCount = conceptCount + sememeCount;
+            log.info("Components to index: " + componentCount);
             conceptService.getParallelConceptChronologyStream().forEach((ConceptChronology<?> conceptChronology) -> {
                     indexers.stream().forEach((i) -> {
-                        i.index(conceptChronology);
+                        //Currently, our indexers expect descriptions, not concepts... though we might want to re-evaluate this...
+                        //I assume that in the future - we will have a description service, rather than embedded descriptions, so leaving as is, for now.
+                        conceptChronology.getConceptDescriptionList().forEach((description) -> {
+                            i.index(description);
+                        });
                     });
                 updateProcessedCount();
             });
             
-            refexProvider.getParallelRefexStream().forEach((RefexMember<?, ?> refex) -> {
-                indexers.stream().forEach((i) -> {
-                    i.index(refex);
-                });
-                updateProcessedCount();
-            });
+            //TODO Dan - not sure if I actually need either of these, any longer... need to test with SOLOR-ALL 
+//            refexProvider.getParallelRefexStream().forEach((RefexMember<?, ?> refex) -> {
+//                indexers.stream().forEach((i) -> {
+//                    i.index(refex);
+//                });
+//                updateProcessedCount();
+//            });
+//            
+//            refexProvider.getParallelDynamicRefexStream().forEach((RefexDynamicChronicleBI<?> refex) -> {
+//                indexers.stream().forEach((i) -> {
+//                    i.index(refex);
+//                });
+//                updateProcessedCount();
+//            });
             
             sememeProvider.getParallelSememeStream().forEach((SememeChronology sememe) -> {
-                indexers.stream().forEach((i) -> {
-                    i.index(sememe);
-                });
-                updateProcessedCount();
+                if (sememe != null)  //TODO Keith -  this IF should not be necessary, but is at the moment, to deal with another bug in sememe provider
+                {
+                    indexers.stream().forEach((i) -> {
+                        i.index(sememe);
+                    });
+                    updateProcessedCount();
+                }
             });
             
             List<IndexStatusListenerBI> islList = LookupService.get().getAllServices(IndexStatusListenerBI.class);
